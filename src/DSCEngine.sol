8fc2a7c6a503105fce21b2be96096cfc0d78c664
Not-Sarthak <notsarthakshah@gmail.com>
2025-03-04 23:38:09 +0530

    deploy

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.28;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
